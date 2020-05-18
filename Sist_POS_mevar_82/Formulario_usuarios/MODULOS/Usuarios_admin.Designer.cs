3ff4d6f0835d500125a7c2deb3ce6536aa3350d5
samybellog144 <56511653+samybellog144@users.noreply.github.com>
2020-05-18 18:24:11 -0500

    Creación de paneles y etiquetas formulario de registro usuarios.

--- Sist_POS_mevar_82/Formulario_usuarios/MODULOS/Usuarios_admin.Designer.cs
+++ Sist_POS_mevar_82/Formulario_usuarios/MODULOS/Usuarios_admin.Designer.cs
@@ -37,16 +37,25 @@
             this.menuStrip_busq_menu_usr = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.panel_der_menu_usr = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pict_box_eliminar_usr = new System.Windows.Forms.PictureBox();
+            this.pict_box_añadir_usr = new System.Windows.Forms.PictureBox();
+            this.gridview_centr_datos_user = new System.Windows.Forms.DataGridView();
+            this.panel_ext_form_reg_usrs = new System.Windows.Forms.Panel();
+            this.lbl_nomb_apell_nuev_usr = new System.Windows.Forms.Label();
+            this.lbl_nom_nuev_usr_reg = new System.Windows.Forms.Label();
+            this.lbl_pwd_nuev_usr_reg = new System.Windows.Forms.Label();
+            this.lbl_email_nuev_usr_reg = new System.Windows.Forms.Label();
+            this.panel_int_form_reg_usrs = new System.Windows.Forms.Panel();
+            this.lbl_rol_nuev_usr_reg = new System.Windows.Forms.Label();
             this.panel_encbz_menu_usr.SuspendLayout();
             this.panel_centr_menu_usr.SuspendLayout();
             this.menuStrip_busq_menu_usr.SuspendLayout();
             this.panel_der_menu_usr.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pict_box_eliminar_usr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pict_box_añadir_usr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridview_centr_datos_user)).BeginInit();
+            this.panel_ext_form_reg_usrs.SuspendLayout();
+            this.panel_int_form_reg_usrs.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_encbz_menu_usr
@@ -116,7 +125,7 @@
             this.menuStrip_busq_menu_usr.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip_busq_menu_usr.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1});
-            this.menuStrip_busq_menu_usr.Location = new System.Drawing.Point(559, 18);
+            this.menuStrip_busq_menu_usr.Location = new System.Drawing.Point(570, 16);
             this.menuStrip_busq_menu_usr.Name = "menuStrip_busq_menu_usr";
             this.menuStrip_busq_menu_usr.Size = new System.Drawing.Size(41, 32);
             this.menuStrip_busq_menu_usr.TabIndex = 1;
@@ -130,45 +139,117 @@
             // 
             // panel_der_menu_usr
             // 
-            this.panel_der_menu_usr.Controls.Add(this.pictureBox2);
-            this.panel_der_menu_usr.Controls.Add(this.pictureBox1);
+            this.panel_der_menu_usr.Controls.Add(this.pict_box_eliminar_usr);
+            this.panel_der_menu_usr.Controls.Add(this.pict_box_añadir_usr);
             this.panel_der_menu_usr.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel_der_menu_usr.Location = new System.Drawing.Point(680, 153);
             this.panel_der_menu_usr.Name = "panel_der_menu_usr";
             this.panel_der_menu_usr.Size = new System.Drawing.Size(200, 294);
             this.panel_der_menu_usr.TabIndex = 2;
             // 
-            // pictureBox2
+            // pict_box_eliminar_usr
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(53, 145);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(84, 81);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pict_box_eliminar_usr.Image = ((System.Drawing.Image)(resources.GetObject("pict_box_eliminar_usr.Image")));
+            this.pict_box_eliminar_usr.Location = new System.Drawing.Point(53, 174);
+            this.pict_box_eliminar_usr.Name = "pict_box_eliminar_usr";
+            this.pict_box_eliminar_usr.Size = new System.Drawing.Size(84, 81);
+            this.pict_box_eliminar_usr.TabIndex = 1;
+            this.pict_box_eliminar_usr.TabStop = false;
             // 
-            // pictureBox1
+            // pict_box_añadir_usr
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(53, 18);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(84, 80);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pict_box_añadir_usr.Image = ((System.Drawing.Image)(resources.GetObject("pict_box_añadir_usr.Image")));
+            this.pict_box_añadir_usr.Location = new System.Drawing.Point(53, 18);
+            this.pict_box_añadir_usr.Name = "pict_box_añadir_usr";
+            this.pict_box_añadir_usr.Size = new System.Drawing.Size(84, 80);
+            this.pict_box_añadir_usr.TabIndex = 0;
+            this.pict_box_añadir_usr.TabStop = false;
             // 
-            // dataGridView1
+            // gridview_centr_datos_user
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 182);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(612, 230);
-            this.dataGridView1.TabIndex = 3;
+            this.gridview_centr_datos_user.AllowUserToAddRows = false;
+            this.gridview_centr_datos_user.AllowUserToResizeRows = false;
+            this.gridview_centr_datos_user.BackgroundColor = System.Drawing.Color.White;
+            this.gridview_centr_datos_user.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridview_centr_datos_user.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridview_centr_datos_user.Location = new System.Drawing.Point(0, 153);
+            this.gridview_centr_datos_user.Name = "gridview_centr_datos_user";
+            this.gridview_centr_datos_user.RowHeadersWidth = 51;
+            this.gridview_centr_datos_user.RowTemplate.Height = 24;
+            this.gridview_centr_datos_user.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridview_centr_datos_user.Size = new System.Drawing.Size(680, 294);
+            this.gridview_centr_datos_user.TabIndex = 3;
+            // 
+            // panel_ext_form_reg_usrs
+            // 
+            this.panel_ext_form_reg_usrs.Controls.Add(this.panel_int_form_reg_usrs);
+            this.panel_ext_form_reg_usrs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel_ext_form_reg_usrs.Location = new System.Drawing.Point(16, 171);
+            this.panel_ext_form_reg_usrs.Name = "panel_ext_form_reg_usrs";
+            this.panel_ext_form_reg_usrs.Size = new System.Drawing.Size(595, 264);
+            this.panel_ext_form_reg_usrs.TabIndex = 4;
+            // 
+            // lbl_nomb_apell_nuev_usr
+            // 
+            this.lbl_nomb_apell_nuev_usr.AutoSize = true;
+            this.lbl_nomb_apell_nuev_usr.Location = new System.Drawing.Point(34, 11);
+            this.lbl_nomb_apell_nuev_usr.Name = "lbl_nomb_apell_nuev_usr";
+            this.lbl_nomb_apell_nuev_usr.Size = new System.Drawing.Size(194, 25);
+            this.lbl_nomb_apell_nuev_usr.TabIndex = 0;
+            this.lbl_nomb_apell_nuev_usr.Text = "Nombres y apellidos:";
+            this.lbl_nomb_apell_nuev_usr.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lbl_nom_nuev_usr_reg
+            // 
+            this.lbl_nom_nuev_usr_reg.AutoSize = true;
+            this.lbl_nom_nuev_usr_reg.Location = new System.Drawing.Point(131, 53);
+            this.lbl_nom_nuev_usr_reg.Name = "lbl_nom_nuev_usr_reg";
+            this.lbl_nom_nuev_usr_reg.Size = new System.Drawing.Size(85, 25);
+            this.lbl_nom_nuev_usr_reg.TabIndex = 0;
+            this.lbl_nom_nuev_usr_reg.Text = "Usuario:";
+            this.lbl_nom_nuev_usr_reg.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lbl_pwd_nuev_usr_reg
+            // 
+            this.lbl_pwd_nuev_usr_reg.AutoSize = true;
+            this.lbl_pwd_nuev_usr_reg.Location = new System.Drawing.Point(96, 92);
+            this.lbl_pwd_nuev_usr_reg.Name = "lbl_pwd_nuev_usr_reg";
+            this.lbl_pwd_nuev_usr_reg.Size = new System.Drawing.Size(120, 25);
+            this.lbl_pwd_nuev_usr_reg.TabIndex = 0;
+            this.lbl_pwd_nuev_usr_reg.Text = "Contraseña:";
+            this.lbl_pwd_nuev_usr_reg.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lbl_email_nuev_usr_reg
+            // 
+            this.lbl_email_nuev_usr_reg.AutoSize = true;
+            this.lbl_email_nuev_usr_reg.Location = new System.Drawing.Point(39, 138);
+            this.lbl_email_nuev_usr_reg.Name = "lbl_email_nuev_usr_reg";
+            this.lbl_email_nuev_usr_reg.Size = new System.Drawing.Size(177, 25);
+            this.lbl_email_nuev_usr_reg.TabIndex = 0;
+            this.lbl_email_nuev_usr_reg.Text = "Correo electrónico:";
+            this.lbl_email_nuev_usr_reg.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // panel_int_form_reg_usrs
+            // 
+            this.panel_int_form_reg_usrs.Controls.Add(this.lbl_pwd_nuev_usr_reg);
+            this.panel_int_form_reg_usrs.Controls.Add(this.lbl_rol_nuev_usr_reg);
+            this.panel_int_form_reg_usrs.Controls.Add(this.lbl_email_nuev_usr_reg);
+            this.panel_int_form_reg_usrs.Controls.Add(this.lbl_nom_nuev_usr_reg);
+            this.panel_int_form_reg_usrs.Controls.Add(this.lbl_nomb_apell_nuev_usr);
+            this.panel_int_form_reg_usrs.Location = new System.Drawing.Point(16, 18);
+            this.panel_int_form_reg_usrs.Name = "panel_int_form_reg_usrs";
+            this.panel_int_form_reg_usrs.Size = new System.Drawing.Size(540, 219);
+            this.panel_int_form_reg_usrs.TabIndex = 1;
+            // 
+            // lbl_rol_nuev_usr_reg
+            // 
+            this.lbl_rol_nuev_usr_reg.AutoSize = true;
+            this.lbl_rol_nuev_usr_reg.Location = new System.Drawing.Point(170, 173);
+            this.lbl_rol_nuev_usr_reg.Name = "lbl_rol_nuev_usr_reg";
+            this.lbl_rol_nuev_usr_reg.Size = new System.Drawing.Size(46, 25);
+            this.lbl_rol_nuev_usr_reg.TabIndex = 0;
+            this.lbl_rol_nuev_usr_reg.Text = "Rol:";
+            this.lbl_rol_nuev_usr_reg.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // Form_menu_usr
             // 
@@ -176,7 +257,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(880, 447);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel_ext_form_reg_usrs);
+            this.Controls.Add(this.gridview_centr_datos_user);
             this.Controls.Add(this.panel_der_menu_usr);
             this.Controls.Add(this.panel_centr_menu_usr);
             this.Controls.Add(this.panel_encbz_menu_usr);
@@ -191,9 +273,12 @@
             this.menuStrip_busq_menu_usr.ResumeLayout(false);
             this.menuStrip_busq_menu_usr.PerformLayout();
             this.panel_der_menu_usr.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pict_box_eliminar_usr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pict_box_añadir_usr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridview_centr_datos_user)).EndInit();
+            this.panel_ext_form_reg_usrs.ResumeLayout(false);
+            this.panel_int_form_reg_usrs.ResumeLayout(false);
+            this.panel_int_form_reg_usrs.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -208,9 +293,16 @@
         private System.Windows.Forms.MenuStrip menuStrip_busq_menu_usr;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.Panel panel_der_menu_usr;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pict_box_añadir_usr;
+        private System.Windows.Forms.PictureBox pict_box_eliminar_usr;
+        private System.Windows.Forms.DataGridView gridview_centr_datos_user;
+        private System.Windows.Forms.Panel panel_ext_form_reg_usrs;
+        private System.Windows.Forms.Label lbl_nomb_apell_nuev_usr;
+        private System.Windows.Forms.Label lbl_nom_nuev_usr_reg;
+        private System.Windows.Forms.Label lbl_email_nuev_usr_reg;
+        private System.Windows.Forms.Label lbl_pwd_nuev_usr_reg;
+        private System.Windows.Forms.Panel panel_int_form_reg_usrs;
+        private System.Windows.Forms.Label lbl_rol_nuev_usr_reg;
     }
 }
 
--- Sist_POS_mevar_82/Formulario_usuarios/MODULOS/Usuarios_admin.cs
+++ Sist_POS_mevar_82/Formulario_usuarios/MODULOS/Usuarios_admin.cs
@@ -26,5 +26,10 @@ namespace Formulario_usuarios
         {
 
         }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
